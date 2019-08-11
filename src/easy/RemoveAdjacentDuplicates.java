69628c54f46f4dd1daf7ef4ae8f1a2b0c7d22908
Renat Kaitmazov <renatkaitmazov@gmail.com>
2019-08-11 19:08:27 +0300

    Optimize RemoveAdjacentDuplicates

--- src/easy/RemoveAdjacentDuplicates.java
+++ src/easy/RemoveAdjacentDuplicates.java
@@ -6,7 +6,7 @@ package easy;
  * We repeatedly make duplicate removals on S until we no longer can.
  * Return the final string after all such duplicate removals have been made.
  * It is guaranteed the answer is unique.
- *
+ * <p>
  * Note:
  * • 1 <= S.length <= 20000
  * • S consists only of English lowercase letters.
@@ -18,22 +18,14 @@ public final class RemoveAdjacentDuplicates {
 
     public String removeDuplicates(String str) {
         final int length = str.length();
-        final StringBuilder builder = new StringBuilder();
-        builder.append(str.charAt(0));
-        for (int i = 1; i < length; ++i) {
+        final char[] stack = new char[length];
+        int stackPointer = 0;
+        for (int i = 0; i < length; ++i) {
             final char currentChar = str.charAt(i);
-            final int builderLength = builder.length();
-            if (builderLength == 0) {
-                builder.append(currentChar);
-                continue;
-            }
-            final char prevChar = builder.charAt(builderLength - 1);
-            if (prevChar == currentChar) {
-                builder.deleteCharAt(builderLength - 1);
-                continue;
-            }
-            builder.append(currentChar);
+            if      (stackPointer == 0) stack[stackPointer++] = currentChar;
+            else if (stack[stackPointer - 1] == currentChar) --stackPointer;
+            else stack[stackPointer++] = currentChar;
         }
-        return builder.toString();
+        return new String(stack, 0, stackPointer);
     }
 }
